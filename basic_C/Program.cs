28f21e8e5025bb7d769e6448f041343dec988d1f
Andrei-GJ <andreisotelo369@gmail.com>
2023-09-19 17:58:20 -0500

    Sync_Project

---
+++ basic_C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace basic_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola care bola");
+            Console.WriteLine(2 + 2);
+            Console.WriteLine(3 + "3");
+            Console.WriteLine("trae el resultado entero");
+            Console.WriteLine(50 / 6);
+            Console.WriteLine("trae el residual ");
+            Console.WriteLine(50 % 6);
+            Console.ReadKey();
+        }
+    }
+}
